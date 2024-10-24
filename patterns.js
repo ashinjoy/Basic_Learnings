794d9f8cc79880e1f52247be4d9160e320436a1b
ashinjoy <ashinjoy22@gmail.com>
2024-10-24 19:03:34 +0530

    star pattern

---
+++ patterns.js
@@ -0,0 +1,89 @@
+// ------------------- STAR-PATTERN -------------------------------------
+//   ****
+//   ****
+//   ****
+//   ****
+// const row = 5
+// for(let i=0; i<row ; i++){
+//     let pattern = ''
+//     for(j=0; j<row ; j++){
+//         pattern += '*'
+//     }
+//     console.log(pattern);
+
+// }
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// const row = 5;
+
+// for (let i = 1; i <= row; i++) {
+//     let pattern = ''
+//   for (let j = 1; j <= i; j++) {
+//     pattern += j
+//   }
+//   console.log(pattern);
+
+// }
+
+// const row = 5
+
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+// const row = 5;
+
+// for (let i = 1; i <= row; i++) {
+//   let pattern = "";
+//   for (let j = 1; j <= i; j++) {
+//     pattern += i;
+//   }
+//   console.log(pattern)
+// }
+
+//  * * * * *
+//  * * * *
+//  * * *
+//  * *
+//  *
+
+// const row = 5;
+
+// for (let i = 5; i > 0; i--) {
+//   let pattern = "";
+//   for (let j = 0; j < i ; j++) {
+//     pattern += "*";
+//   }
+//   console.log(pattern);
+// }
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+// const row = 5;
+
+// for (let i = 5; i > 0; i--) {
+//   let pattern = "";
+//   for (j = 1; j <= i; j++) {
+//     pattern += j;
+//   }
+//   console.log(pattern);
+  
+// }
+
+//        *
+//     *  *  *
+//  *  *  *  *  *
+// *   *  *  *   *  *
+
+
